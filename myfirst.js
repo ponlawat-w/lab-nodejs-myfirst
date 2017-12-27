46b6adc90c42bd760ba32497d2a2c5a0a811a993
Ponlawat <ponlawat_w@cmu.ac.th>
2017-12-27 16:53:01 +0700

    date module

--- myfirst.js
+++ myfirst.js
@@ -1,7 +1,11 @@
 var http = require('http');
+var date = require('./date');
 
 http.createServer(function (request, response)
 {
     response.writeHead(200, {'Content-Type': 'text/html'});
-    response.end('Hello World!');
+    response.write('<meta charset="utf-8">')
+    response.write('<h2>Current Date and Time</h2>');
+    response.write('ขณะนี้เวลา:' + date.currentDate());
+    response.end();
 }).listen(8080);
